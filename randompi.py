00c30ed4a73f74ce5f1943c58cc0515f4e70d693
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-03-13 20:28:38 -0400

    Made the core logic callable, implemented some tests.

--- randompi.py
+++ randompi.py
@@ -4,15 +4,15 @@ import sys
 import fractions
 
 # run_calculation takes a number of iterations to perform and then runs that many
-# random number coprime comparisons. It returns a value for pi, and will print
-# out the results. It also optionally takes in a maximum random number, which defaults
-# to sys.maxsize.
-def run_calculation(iterations, max_random_number = sys.maxsize):
+# random number coprime comparisons. It returns a value for pi. It also optionally 
+# takes in a maximum random number, which defaults to sys.maxsize.
+# If the verbose flag is set to true, it will print out the indepth results of the computation
+def run_calculation(iterations, max_random_number = sys.maxsize, verbose = False):
 
     coprimes = 0
     cofactors = 0
 
-    for i in range(iterations):
+    for i in range(1, iterations):
         try:
             x , y = generate_two_numbers(max_random_number)
             
@@ -24,22 +24,26 @@ def run_calculation(iterations, max_random_number = sys.maxsize):
                 cofactors += 1
 
             #report what number we're on every 100000 iterations
-            if i % 100000 == 0:
-                print("Currently on {}".format(i))
+            #if i % 100000 == 0:
+            #    print("Currently on {}".format(i))
 
         except KeyboardInterrupt:
-            break
+            if __name__ == "__main__":
+                break
+            else:
+                raise KeyboardInterrupt
 
     total = coprimes + cofactors
     coprime_probability = coprimes / total
     pi_estimate = math.sqrt(6/coprime_probability)
 
-    print()
-    print("        Total Tries:\t{}".format(total))
-    print("           Coprimes:\t{}".format(coprimes))
-    print("          Cofactors:\t{}".format(cofactors))
-    print("Coprime probability:\t{}".format(coprime_probability))
-    print("        Pi Estimate:\t{}".format(pi_estimate))
+    if verbose:
+        print()
+        print("        Total Tries:\t{}".format(total))
+        print("           Coprimes:\t{}".format(coprimes))
+        print("          Cofactors:\t{}".format(cofactors))
+        print("Coprime probability:\t{}".format(coprime_probability))
+        print("        Pi Estimate:\t{}".format(pi_estimate))
 
     return pi_estimate
 
@@ -47,7 +51,8 @@ def generate_two_numbers(max_random_number):
     return (random.randint(1, max_random_number), random.randint(1, max_random_number))
 
 def main():
-    run_calculation(sys.maxsize)
+    run_calculation(sys.maxsize, verbose = True)
 
 if __name__ == "__main__":
     main()
+    
---
+++ runtests.py
@@ -0,0 +1,48 @@
+"""
+This is just a way to see which method alluded to in the README will produce the
+best result.
+
+Think of it as a test, I guess.
+"""
+import randompi
+
+# I cannot believe I had to implement this myself
+def average(list_of_values):
+    return sum(list_of_values) / len(list_of_values)
+
+# Basically, I'm going to make a bunch of lists holding a thousand (1000) of values of pi as
+# calculated via various parameters.
+
+# Baseline, method works as shown in the original video.
+# Iterations = 500
+# Max random value = 120
+baseline = [randompi.run_calculation(501, max_random_number=121) for i in range(1000)]
+
+baseline_average = average(baseline)
+
+print("                    Baseline average estimation for pi:\t{}".format(baseline_average))
+
+#Baseline, but done a million times
+# Iterations = 1000000
+# Max random value = 120
+million_baseline = [randompi.run_calculation(1000001, max_random_number=121) for i in range(1000)]
+
+million_baseline_average = average(million_baseline)
+
+print("            Million baseline average estimation for pi:\t{}".format(baseline_average))
+
+# Iterations = 500
+# Default max value (sys.maxsize)
+baseline_max_value = [randompi.run_calculation(501) for i in range(1000)]
+
+baseline_max_value_average = average(baseline_max_value)
+
+print("      500 tries at max value average estimation for pi:\t{}".format(baseline_max_value_average))
+
+# Iterations = 1000000
+# Default max value (sys.maxsize)
+million_max_value = [randompi.run_calculation(1000001) for i in range(1000)]
+
+million_max_value_average = average(million_max_value)
+
+print("A million tries at max value average estimation for pi:\t{}".format(baseline_average))
