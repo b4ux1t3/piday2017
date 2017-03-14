c521d7bfc93647c415c78bf875ff4b96dbb33d98
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-03-14 13:14:16 -0400

    Switched to the statistics library. Implemented a timer for execution

--- runtests.py
+++ runtests.py
@@ -5,10 +5,12 @@ best result.
 Think of it as a test, I guess.
 """
 import randompi
+import statistics
+import time
 
-# I cannot believe I had to implement this myself
-def average(list_of_values):
-    return sum(list_of_values) / len(list_of_values)
+print("Starting timer")
+
+start_time = time.time()
 
 # Basically, I'm going to make a bunch of lists holding a thousand (1000) of values of pi as
 # calculated via various parameters.
@@ -18,31 +20,39 @@ def average(list_of_values):
 # Max random value = 120
 baseline = [randompi.run_calculation(501, max_random_number=121) for i in range(1000)]
 
-baseline_average = average(baseline)
+baseline_average = statistics.mean(baseline)
 
 print("                    Baseline average estimation for pi:\t{}".format(baseline_average))
+print("                                        Execution time:\t{}".format(time.time() - start_time))
+elapsed_time = time.time()
 
 #Baseline, but done a million times
 # Iterations = 1000000
 # Max random value = 120
 million_baseline = [randompi.run_calculation(1000001, max_random_number=121) for i in range(1000)]
 
-million_baseline_average = average(million_baseline)
+million_baseline_average = statistics.mean(million_baseline)
 
 print("            Million baseline average estimation for pi:\t{}".format(million_baseline_average))
+print("                                        Execution time:\t{}".format(time.time() - elapsed_time))
+elapsed_time = time.time()
 
 # Iterations = 500
 # Default max value (sys.maxsize)
 baseline_max_value = [randompi.run_calculation(501) for i in range(1000)]
 
-baseline_max_value_average = average(baseline_max_value)
+baseline_max_value_average = statistics.mean(baseline_max_value)
 
 print("      500 tries at max value average estimation for pi:\t{}".format(baseline_max_value_average))
+print("                                        Execution time:\t{}".format(time.time() - elapsed_time))
+elapsed_time = time.time()
 
 # Iterations = 1000000
 # Default max value (sys.maxsize)
 million_max_value = [randompi.run_calculation(1000001) for i in range(1000)]
 
-million_max_value_average = average(million_max_value)
+million_max_value_average = statistics.mean(million_max_value)
 
 print("A million tries at max value average estimation for pi:\t{}".format(million_max_value_average))
+print("                                        Execution time:\t{}".format(time.time() - elapsed_time))
+elapsed_time = time.time()
